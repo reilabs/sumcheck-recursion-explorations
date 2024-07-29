84163a210f5de27effce1b9c42dba9a0def4bdfe
Batmend Batsaikhan <batmendbar@gmail.com>
2024-07-29 18:22:33 -0400

    Increased test function variables

--- main.go
+++ main.go
@@ -31,12 +31,12 @@ func checkMiddleRounds (api frontend.API, circuit *Circuit, hashes *[]frontend.V
     var poseidonInstance = poseidon.NewPoseidon(api)
     poseidonInstance.Write(circuit.Seed)
     poseidonInstance.Write(circuit.C1)
-    var hash = poseidonInstance.Sum()
     for i:=1; i<len(circuit.GiCoeffs); i++ {
-        poseidonInstance.Write(hash)
         poseidonInstance.Write(circuit.GiCoeffs[i-1]...)
-        hash = poseidonInstance.Sum()
+        hash := poseidonInstance.Sum()
+        api.Println(hash)
         *hashes = append(*hashes, hash)
+        poseidonInstance.Write(hash)
         evalRnd := polynomials.UniP(circuit.GiCoeffs[i-1], hash, api)
         check01(api, evalRnd, circuit.GiCoeffs[i])
     }
@@ -63,23 +63,27 @@ func (circuit *Circuit) Define (api frontend.API) error {
 }
 
 func main() {
-    var GiCoeffs = make([][]frontend.Variable, 2)
-    for i:=0;i<2;i++ {
+    var GiCoeffs = make([][]frontend.Variable, 3)
+    for i:=0;i<3;i++ {
         GiCoeffs[i] = make([]frontend.Variable, 2)
     }
     var circuit = Circuit{
-        GCoeffs: make([]frontend.Variable, 4),
+        GCoeffs: make([]frontend.Variable, 8),
         GiCoeffs: GiCoeffs,
     }
     ccs, _ := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
     pk, vk, _ := groth16.Setup(ccs)
-    G2_0, _ := new(big.Int).SetString("57641245413269694071184691489861811328223123049926456696620017428154679498982", 10)
-    G2_1, _ := new(big.Int).SetString("192137484710898980237282304966206037760743743499754855655400058093848931663277", 10)
+    G1_0, _ := new(big.Int).SetString("1020204", 10)
+    G1_1, _ := new(big.Int).SetString("10202040", 10)
+    G2_0, _ := new(big.Int).SetString("482480799671326968966589061680369608414568155496865577406024069086729633310015242", 10)
+    G2_1, _ := new(big.Int).SetString("48248079967132696896658906168036960841456815549686557740602406908672963331001524200", 10)
+    G3_0, _ := new(big.Int).SetString("88703303929150795986606904465882177409725616560450802144675156296904524300670214609344006370093441889857321811758012908626046938167740756304280941713422321", 10)
+    G3_1, _ := new(big.Int).SetString("887033039291507959866069044658821774097256165604508021446751562969045243006702146093440063700934418898573218117580129086260469381677407563042809417134223210000", 10)
     assignment := Circuit{ 
         Seed: 47,
-        C1: 34,
-        GCoeffs: []frontend.Variable{1,3,7,10}, //evalRnd(x, y) := 1 + 3x + 7y + 10xy
-        GiCoeffs: [][]frontend.Variable{[]frontend.Variable{9,16}, []frontend.Variable{G2_0, G2_1}},
+        C1: 12242448,
+        GCoeffs: []frontend.Variable{1,10,100,1000,10000, 100000, 1000000, 10000000}, //evalRnd(x, y) := 1 + 3x + 7y + 10xy
+        GiCoeffs: [][]frontend.Variable{[]frontend.Variable{G1_0, G1_1}, []frontend.Variable{G2_0, G2_1}, []frontend.Variable{G3_0, G3_1}},
     }
     witness, _ := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
     publicWitness, _ := witness.Public()
